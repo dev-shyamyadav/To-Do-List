d08f0eae68301b7bc1f8843763af3a69efe309e4
dev-shyamyadav <work4shyamyadav@gmail.com>
2024-05-04 00:03:31 +0530

    Add buttons for task

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+let taskList = document.querySelector(".task-list");
 let inputField = document.querySelector(".input-field");
 let addButton = document.querySelector(".add-button");
 
@@ -7,10 +8,40 @@ let addTask = addButton.addEventListener("click", (event) => {
     if (taskText === "") {
         alert( "Please enter a task" );
     } else {
+        //Create task div
         let newTask = document.createElement("div");
         newTask.className = "task";
-        newTask.textContent = taskText
-        document.querySelector(".task-list").appendChild(newTask);
+        taskList.appendChild(newTask);
+
+        //Create text for task
+        let textContent = document.createElement("div");
+        textContent.innerHTML = taskText;
+        newTask.appendChild(textContent);
         inputField.value = "";
+
+        //Create button div
+        let taskButtons = document.createElement("div");
+        taskButtons.className = "task-buttons";
+        newTask.appendChild(taskButtons);
+
+        //Create complete button
+        let completeButton = document.createElement("button");
+        completeButton.className = "complete-button";
+        completeButton.innerHTML = "<i class='fa-solid fa-check'></i>";
+        taskButtons.appendChild(completeButton);
+
+        //Create edit button
+        let editButton = document.createElement("button");
+        editButton.className = "edit-button";
+        editButton.innerHTML = "<i class='fa-solid fa-pen'></i>";
+        taskButtons.appendChild(editButton);
+
+        //Create delete button
+        let deleteButton = document.createElement("button");
+        deleteButton.className = "delete-button";
+        deleteButton.innerHTML = "<i class='fa-solid fa-trash'></i>";
+        taskButtons.appendChild(deleteButton);
+
+
     }
 })
